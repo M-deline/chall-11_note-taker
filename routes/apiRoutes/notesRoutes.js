80c8ab1fa6a6ed4f5c44a686f946721d872d829e
Madeline K <madkovanda@gmail.com>
2023-10-25 23:03:54 -0600

    trying to fix get issue

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,26 +1,15 @@
 const router = require('express').Router();
-const { notes } = require('../../db/db.json')   
+const { notes } = require('../../db/db')   
 const { makeNewNote } = require('../../lib/notes');
 
-// const cors = require('cors');
-// const { arrayNotes } = require('../../db/db.json')
-//SOMETHING WRONG WITH GET routers
-// router.get("/", (req, res) => {
-//     res.json(notes);
-//     res.status(200).json({status: '1', message: 'Success'});
-// }
-// );
-//says to replace with text instead of json 
-// The DOCTYPE response is indicative of you receiving HTML rather than a JSON response. 
+
 router.get('/notes', (req, res) => {
    let results = notes;
-   res.json (results);
+//    JSON.parse(results);
+    res.json(results);
     });
-
+//router get Promise (route(req))
 router.post('/notes', (req, res) => {
-    // req.body.id = arrayNotes.length.toString();
-    // let note = makeNewNote(req.body, notes);
-    // res.json(note);
     if(notes){
         req.body.id = notes.length.toString();
     } else {
